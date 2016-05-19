25f0130bb631f67491af9e477910e2dd4373f6f3
Kenneth Kousen <ken.kousen@kousenit.com>
2016-05-19 16:12:14 -0500

    Updating version

--- src/test/groovy/mjg/HelloSpockSpec.groovy
+++ src/test/groovy/mjg/HelloSpockSpec.groovy
@@ -2,6 +2,7 @@ package mjg
 
 import spock.lang.Specification
 import spock.lang.Unroll;
+
 /*
  * Copyright 2009 the original author or authors.
  *
@@ -19,30 +20,32 @@ import spock.lang.Unroll;
  */
 //@Unroll
 class HelloSpockSpec extends Specification {
-	@Unroll
-	def "#name should have #length"(String name, int length) {
-		expect:
-		name.size() == length
-
-		where:
-		name     | length
-		"Spock"  | 5
-		"Kirk"   | 4
-		"Scotty" | 6
-	}
-
-	def "check lengths using arrays"() {
-		expect: name.size() == length
+    @Unroll
+    def "#name should have #length"(String name, int length) {
+        expect:
+        name.size() == length
 
         where:
-		name << ["Spock","Kirk","Scotty"]
-		length << [5,4,6]
-	}
-	
+        name     | length
+        "Spock"  | 5
+        "Kirk"   | 4
+        "Scotty" | 6
+    }
+
+    def "check lengths using arrays"() {
+        expect:
+        name.size() == length
+
+        where:
+        name << ["Spock", "Kirk", "Scotty"]
+        length << [5, 4, 6]
+    }
+
     @Unroll
-	def "check #length using #name pairs"() {
-		expect: name.size() == length
-		where:
-		[name,length] << [["Spock",5],["Kirk",4],["Scotty",6]]
-	}
+    def "check #length using #name pairs"() {
+        expect:
+        name.size() == length
+        where:
+        [name, length] << [["Spock", 5], ["Kirk", 4], ["Scotty", 6]]
+    }
 }
