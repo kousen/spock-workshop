c6456aedf78d1c4b0de5de20e0d6ca92e6799096
Kenneth Kousen <ken.kousen@kousenit.com>
2016-11-27 15:26:10 -0500

    Add solutions to PersonSpec tests

--- src/test/groovy/mjg/PersonSpec.groovy
+++ src/test/groovy/mjg/PersonSpec.groovy
@@ -8,33 +8,46 @@ class PersonSpec extends Specification {
     def 'name uses first and last'() {
         given:
         // instantiate a person using tuple constructor
+        Person p = new Person(first: 'Jean-Luc', last: 'Picard')
 
         expect:
         // check that the person's name equals first + last
-        true // remove this line when you add your test
+        p.name == 'Jean-Luc Picard'
     }
 
     def 'check equals and hashCode using a Set'() {
         given:
         // create two persons with the same attributes
+        Person p1 = new Person(first: 'Jean-Luc', last: 'Picard')
+        Person p2 = new Person(first: 'Jean-Luc', last: 'Picard')
 
         when:
         // add them to a Set
-        true
+        Set people = [p1, p2]
 
         then:
         // check that the persons are equal
-        // check that the size of the Set is only one
-        true // remove this line when you add your test
-    }
-    
-    @Unroll
-    def '#person.name from #first and #last'() {
-        expect:
-        // test that the person's name is equal to first + last
+        p1 == p2
 
-        // where:
+        // check that the size of the Set is only one
+        people.size() == 1
+    }
+
+    @Unroll
+    def '#name from #first and #last'() {
+        when:
+        // instantiate a person using first and last
+        Person p = new Person(first: first, last: last)
+
+        then:
+        // test that the person's name is equal to first + last
+        p.name == name
+
+        where:
         // use a data table to create a person from first and last
-        true // remove this line when you add your test
+        first      | last      || name
+        'James'    | 'Kirk'    || 'James Kirk'
+        'Benjamin' | 'Sisko'   || 'Benjamin Sisko'
+        'Kathryn'  | 'Janeway' || 'Kathryn Janeway'
     }
 }
