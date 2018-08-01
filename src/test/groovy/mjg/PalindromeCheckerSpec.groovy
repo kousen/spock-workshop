1e5c42c32322f8e0be74c748a6c23e236a2ad8da
Ken Kousen <ken.kousen@kousenit.com>
2018-08-01 09:46:32 -0400

    Add JUnit 5 dependency to build

--- src/test/groovy/mjg/PalindromeCheckerSpec.groovy
+++ src/test/groovy/mjg/PalindromeCheckerSpec.groovy
@@ -6,21 +6,31 @@ import spock.lang.Specification
 class PalindromeCheckerSpec extends Specification {
     PalindromeChecker checker = new PalindromeChecker()
 
-    def palindromes = [
+    List<String> palindromes = [
             'racecar',
             'A Santa pets rats, as Pat taps a star step at NASA.',
             'Do geese see God?',
-            'Flee to me, remote elf!'
-    ]
+            'Flee to me, remote elf!']
 
     @Before
     void start() {
-        println 'pre-processing...'
+        println 'using @Before...'
+    }
+
+    void setup() {
+        println 'using setup method...'
     }
 
     def "all the listed strings are palindromes"() {
         expect:
         palindromes.every { str -> checker.isPalindrome(str) }
+    }
+
+    def "using each returns true even if it shouldn't"() {
+        expect:
+        palindromes.each { str ->
+            checker.isPalindrome(str) // assert required!
+        }
     }
 
     def 'all listed strings are palindromes (alternative way)'() {
