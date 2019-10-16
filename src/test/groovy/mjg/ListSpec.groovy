76510431215daf48ff17672ba60ebbbd6b6fc084
Ken Kousen <ken.kousen@kousenit.com>
2019-10-16 18:56:23 -0400

    Update versions and reset to starter

--- src/test/groovy/mjg/ListSpec.groovy
+++ src/test/groovy/mjg/ListSpec.groovy
@@ -8,22 +8,20 @@ class ListSpec extends Specification {
     
     def setup() {
         // initialize the strings var to hold six strings
-        strings = 'this is a list of strings'.split()
     }
 
     def "there are six strings"() {
         // check that there are six strings
-        expect: strings.size() == 6
+        expect: true
     }
     
     def "append a string"() {
         when:
         // use the left shift operator to append a new element
-        strings << 'extra'
 
         then:
         // check that the new size is the old size + 1
-        strings.size() == old(strings.size()) + 1
+        true
     }
     
     def "NPE if I don't instantiate the list"() {
@@ -32,22 +30,20 @@ class ListSpec extends Specification {
 
         when:
         // append a new element to 'empty'
-        empty << 'abc'
 
         then:
         // check that a NullPointerException is thrown
-        thrown(NullPointerException)
+        true
     }
     
     def 'no exception even beyond the end of the list'() {
         when:
         // access each element of the list
         // use an index beyond the end of the list
-        strings[99]
 
         then:
         // verify that NO exceptions are thrown
         // notThrown(NullPointerException)
-        noExceptionThrown()
+        true
     }
 }
